be228245597aec3d34fcd1c1c58356b6f4bf9f46
maginodan <137385172+maginodan@users.noreply.github.com>
2025-07-16 01:35:09 +0300

    Update home-carousel.tsx

--- components/shared/home/home-carousel.tsx
+++ components/shared/home/home-carousel.tsx
@@ -13,12 +13,15 @@ import {
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
+import { useTranslations } from 'next-intl'
 import { ICarousel } from '@/types'
 
 export function HomeCarousel({ items }: { items: ICarousel[] }) {
   const plugin = React.useRef(
     Autoplay({ delay: 3000, stopOnInteraction: true })
   )
+
+  const t = useTranslations('Home')
 
   return (
     <Carousel
@@ -46,10 +49,10 @@ export function HomeCarousel({ items }: { items: ICarousel[] }) {
                       'text-xl md:text-6xl font-bold mb-4 text-primary  '
                     )}
                   >
-                    {item.title}
+                    {t(`${item.title}`)}
                   </h2>
                   <Button className='hidden md:block'>
-                    {item.buttonCaption}
+                    {t(`${item.buttonCaption}`)}
                   </Button>
                 </div>
               </div>
